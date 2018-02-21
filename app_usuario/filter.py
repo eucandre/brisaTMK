5f5cc935939093d4f70f63f69890ce722550d33d
eucandre <eucandre@gmail.com>
2018-02-21 15:49:27 -0300

    ajustes

--- app_usuario/filter.py
+++ app_usuario/filter.py
@@ -4,8 +4,8 @@ from .models import User
 from django import forms
 class ColaboradorFilter(django_filters.FilterSet):
     username = django_filters.CharFilter(label='Usuario',lookup_expr='icontains', widget=forms.TextInput(attrs={'class':'form-control'}))
-    telphone = django_filters.CharFilter(label='Usuario',lookup_expr='icontains', widget=forms.TextInput(attrs={'class':'form-control'}))
-    office = django_filters.CharFilter(label='Usuario',lookup_expr='icontains', widget=forms.TextInput(attrs={'class':'form-control'}))
+    telphone = django_filters.CharFilter(label='Telefone',lookup_expr='icontains', widget=forms.TextInput(attrs={'class':'form-control'}))
+    office = django_filters.CharFilter(label='Cargo',lookup_expr='icontains', widget=forms.TextInput(attrs={'class':'form-control'}))
     class Meta:
         model = User
         fields = ['username', 'telphone', 'office', ]
